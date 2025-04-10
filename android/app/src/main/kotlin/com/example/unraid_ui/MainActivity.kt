cd16f2248b83233671deae3bbe43dafee684ebc4
S3ppo <harald@moniholz.at>
2025-04-10 21:36:58 +0200

    cleanup

--- android/app/src/main/kotlin/com/example/unraid_ui/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.hw.unraidui
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/hw/unraid_ui/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.hw.unraid_ui
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
